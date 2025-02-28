aca86ffcad16eea361c9cdd8d10d6a3605c227c9
sabbirafmkhan <sabbirafmkhan@gmail.com>
2025-02-28 20:22:25 +0600

    structure file folder

---
+++ lib/screens/ProductCreateScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProductCreateScreen extends StatefulWidget {
+  const ProductCreateScreen({super.key});
+
+  @override
+  State<ProductCreateScreen> createState() => _ProductCreateScreenState();
+}
+
+class _ProductCreateScreenState extends State<ProductCreateScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
